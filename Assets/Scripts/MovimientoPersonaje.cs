5ceb560378d75329201e552ba7a6dcdbab251d72
juanisarasa01 <61529548+juanisarasa01@users.noreply.github.com>
2021-10-04 17:20:04 -0300

    update

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -19,10 +19,14 @@ public class MovimientoPersonaje : MonoBehaviour
         if (Input.GetButton("Derecha"))
         {
             move.x = 1;
+            gameObject.GetComponent<Animator>().SetBool("run", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
         if (Input.GetButton("Izquierda"))
         {
             move.x = -1;
+            gameObject.GetComponent<Animator>().SetBool("run", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
         if (Input.GetButton("Arriba"))
         {
@@ -37,6 +41,11 @@ public class MovimientoPersonaje : MonoBehaviour
             Instantiate(objetoACrear, transform.position, transform.rotation);
         }
             rb2d.MovePosition((Vector2)this.transform.position + (move * Velocidad * Time.deltaTime)); 
+
+        if (!Input.GetButton("Derecha") && !Input.GetButton("Izquierda"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("run", false);
+        }
             
     }
 }
