aea834457f7f077f855906b71eecaf281fdf807e
AleHutmacher <91856761+AleHutmacher@users.noreply.github.com>
2021-10-03 22:06:12 -0300

    Proyecto

---
+++ Proyecto Wizards/Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float Velocidad = 1.0f;
+    private Rigidbody2D rb2d;
+    public GameObject objetoACrear;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+        if (Input.GetButton("Derecha"))
+        {
+            move.x = 1;
+        }
+        if (Input.GetButton("Izquierda"))
+        {
+            move.x = -1;
+        }
+        if (Input.GetButton("Arriba"))
+        {
+            move.y = 1;
+        }
+        if (Input.GetButton("Abajo"))
+        {
+            move.y = -1;
+        }
+        if (Input.GetButtonUp("Disparar"))
+        {
+            Instantiate(objetoACrear, transform.position, transform.rotation);
+        }
+            rb2d.MovePosition((Vector2)this.transform.position + (move * Velocidad * Time.deltaTime)); 
+            
+    }
+}
